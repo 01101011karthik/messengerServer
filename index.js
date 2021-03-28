d8d71786ca33970d3ca6d25cac732f21150dfb69
01101011karthik <o11o1o11.o11o1o11@gmail.com>
2021-03-28 13:22:10 +0530

    added changes

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const CORS = require('cors');
-const http = require('http').Server(app)
+const dotenv = require('dotenv');
+const http = require('http').Server(app);
+
 
 const io = require('socket.io')(http, {
     cors: {
@@ -9,14 +12,18 @@ const io = require('socket.io')(http, {
     }
 });
 
-io.use((socket, next) => {
-    const username = socket.handshake.auth.userName;
-    if (!username) {
-      return next(new Error("invalid username"));
-    }
-    socket.username = username;
-    next();
-});
+//config
+dotenv.config({path: path.resolve(__dirname, './.env')})
+const port = process.env.PORT;
+
+// io.use((socket, next) => {
+//     const username = socket.handshake.auth.userName;
+//     if (!username) {
+//       return next(new Error("invalid username"));
+//     }
+//     socket.username = username;
+//     next();
+// });
 
 app.use(CORS({
     origin: ['http://localhost:3000', 'https://sockettypemessengerapp.herokuapp.com'],
@@ -32,48 +39,48 @@ app.get('/clear', (req, res) => {
     res.send(loggedInUsers)
 })
 
-io.on('connection', (socket) => {
-    let loggedInUsers = [];
+// io.on('connection', (socket) => {
+//     let loggedInUsers = [];
 
-    for (let [id, socket] of io.of("/").sockets) {
-        loggedInUsers.push({
-            userID: id,
-            username: socket.username,
-            messages: [],
-            unreadMessagesCount: 0
-        });
-    }
-    io.emit('getActiveUsers', loggedInUsers)
+//     for (let [id, socket] of io.of("/").sockets) {
+//         loggedInUsers.push({
+//             userID: id,
+//             username: socket.username,
+//             messages: [],
+//             unreadMessagesCount: 0
+//         });
+//     }
+//     io.emit('getActiveUsers', loggedInUsers)
 
-    socket.broadcast.emit("user connected", {
-        userID: socket.id,
-        username: socket.username,
-    });
+//     socket.broadcast.emit("user connected", {
+//         userID: socket.id,
+//         username: socket.username,
+//     });
 
-    socket.on('userTyping', ({isTyping, to}) => {
-        socket.to(to).emit("userTyping", {
-            isTyping,
-            from: socket.id
-        })
-    })
+//     socket.on('userTyping', ({isTyping, to}) => {
+//         socket.to(to).emit("userTyping", {
+//             isTyping,
+//             from: socket.id
+//         })
+//     })
     
 
-    socket.on('message', ({payload, to}) => {
-        socket.to(to).emit("message", {
-            payload,
-            fromSelf: false,
-            from: socket.id
-        })
-    })
+//     socket.on('message', ({payload, to}) => {
+//         socket.to(to).emit("message", {
+//             payload,
+//             fromSelf: false,
+//             from: socket.id
+//         })
+//     })
 
     
-    socket.on("disconnect", () => {
-        console.log("Client disconnected");
-        const filteredUsers = loggedInUsers.filter(item => item.userID !== socket.id)
+//     socket.on("disconnect", () => {
+//         console.log("Client disconnected");
+//         const filteredUsers = loggedInUsers.filter(item => item.userID !== socket.id)
 
-        loggedInUsers = filteredUsers;
-        io.emit('getActiveUsers', loggedInUsers)
-    });
-})
+//         loggedInUsers = filteredUsers;
+//         io.emit('getActiveUsers', loggedInUsers)
+//     });
+// })
 
 http.listen(5000, () => console.log('listening on 3000'))
